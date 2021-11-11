4a9e8272f0291030a423a3222d562e7de26513c0
armin <a.shoeibi@alibaba.ir>
2021-11-11 20:56:27 +0330

    Add project files.

---
+++ src/Ticketing.DTOs/CreateTicketDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ticketing.DTOs;
+
+public record class CreateTicketDto : IValidatableObject
+{
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (string.IsNullOrEmpty(PhoneNumber) && string.IsNullOrEmpty(Email) is false ||
+            string.IsNullOrEmpty(Email) && string.IsNullOrEmpty(PhoneNumber) is false)
+        {
+            string phoneNumber = nameof(PhoneNumber);
+            string email = nameof(Email);
+            yield return new ValidationResult($"{phoneNumber} and {email} must be either null or have value.");
+        }
+    }
+}
---
+++ tests/Ticketing.DTOs.Tests/CreateTicketDtoTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Xunit;
+
+namespace Ticketing.DTOs.Tests;
+
+public class CreateTicketDtoTests
+{
+
+    [Theory]
+    [InlineData("+989000000000", "armin@gmail.com")]
+    [InlineData(null, null)]
+    public void No_Exception_Thrown_When_PhoneNumber_And_Email_Has_Value_Or_Null(string phoneNumber, string email)
+    {
+        // Arrange
+        CreateTicketDto createTicketDto = new()
+        {
+            PhoneNumber = phoneNumber,
+            Email = email,
+        };
+        // Act
+        Exception recordedException = Record.Exception(() => Validator.ValidateObject(createTicketDto, new(createTicketDto)));
+
+        // Assert
+        Assert.Null(recordedException);
+    }
+
+    [Theory]
+    [InlineData("+989000000000", null)]
+    [InlineData(null, "armin@gmail.com")]
+    public void Exception_Thrown_When_PhoneNumber_Or_Email_Is_Null(string phoneNumber, string email)
+    {
+        // Arrange
+        CreateTicketDto createTicketDto = new()
+        {
+            PhoneNumber = phoneNumber,
+            Email = email,
+        };
+        // Act & Assert
+        Assert.Throws<ValidationException>(() => Validator.ValidateObject(createTicketDto, new(createTicketDto)));
+    }
+
+
+}
